22e2205ccc68ebda42c9b5c524bb1e3bdfc15657
Paul Hudlow <paul.hudlow@gmail.com>
2016-03-30 22:35:26 -0400

    Add user data access class.

--- EclipseWorkspace/CompanyCalendar/DataAccess/UserData.php
+++ EclipseWorkspace/CompanyCalendar/DataAccess/UserData.php
@@ -13,4 +13,20 @@
 	{
 		return new User;
 	}
+	
+	class UserDataAccessor
+	{	
+		static function GetUserByUserName($userName)
+		{
+			$database = DatabaseConnector::GetDatabase();
+			$returnUser = new User;
+			$data = $database->select("user", "*", array("user_name" => $userName));
+			$returnUser->userName = $data[0]["user_name"];
+			$returnUser->firstName = $data[0]["first_name"];
+			$returnUser->lastName = $data[0]["last_name"];
+			$returnUser->isHR = $data[0]["is_hr"];
+			
+			return $returnUser;
+		}
+	}
 ?>
